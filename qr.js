55fd90e8418ee6a90dbe70abc4da9a5f61fa1d52
kingtech12 <kervensleysaintfleur52@gmail.com>
2025-06-13 02:49:55 -0400

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,169 @@
+const { makeid } = require('./gen-id');
+const express = require('express');
+const QRCode = require('qrcode');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers,
+    jidNormalizedUser
+} = require('@whiskeysockets/baileys');
+const { upload } = require('./mega');
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+router.get('/', async (req, res) => {
+    const id = makeid();
+ //   let num = req.query.number;
+    async function K1NG_XMD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/' + id);
+        try {
+var items = ["Safari"];
+function selectRandomItem(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+var randomItem = selectRandomItem(items);
+            
+            let sock = makeWASocket({
+                	
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+            
+            sock.ev.on('creds.update', saveCreds);
+            sock.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect,
+                    qr
+                } = s;
+              if (qr) await res.end(await QRCode.toBuffer(qr));
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    let rf = __dirname + `/temp/${id}/creds.json`;
+                    function generateRandomText() {
+                        const prefix = "3EB";
+                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        let randomText = prefix;
+                        for (let i = prefix.length; i < 22; i++) {
+                            const randomIndex = Math.floor(Math.random() * characters.length);
+                            randomText += characters.charAt(randomIndex);
+                        }
+                        return randomText;
+                    }
+                    const randomText = generateRandomText();
+                    try {
+                        const { upload } = require('./mega');
+                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        let md = "K1NG~XMD~" + string_session;
+                        let code = await sock.sendMessage(sock.user.id, { text: md });
+                        let desc = `*_QR  Code Connected *
+                        
+🧠 SYSTEM STATUS: ONLINE
+⫸ K1NG-XMD ⛓️ DEPLOYED SUCCESSFULLY
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+📍 OPERATION LOG:
+✔️ INITIATION COMPLETE
+✔️ BOT FRAMEWORK INJECTED
+✔️ CYBER CORE: STABLE
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+👁‍🗨 SUPPORT HUB:
+⮞ YOUTUBE: youtube.com/@K1NGTECH
+⮞ OWNER LINK: wa.me/50933734591
+⮞ SOURCE: github.com/DEVK1NG/K1NG-XMD_V1
+⮞ WA CHANNEL: https://whatsapp.com/channel/0029VbB1ik6JJhzOJmJQI33d
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⭐ STAR THE REPO TO SUPPORT THE CODEVERSE
+💻 BUILT WITH 💙 BY DEV K1NG
+
+> *© 𝐊1𝐍𝐆-𝐗𝐌𝐃*`; 
+                        await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: " 𝖇𝖔𝖙 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉",
+thumbnailUrl: "https://files.catbox.moe/4v0adk.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029VbB2Xry0LKZAuosqRO0k",
+mediaType: 1,
+renderLargerThumbnail: true
+}  
+}
+},
+{quoted:code })
+                    } catch (e) {
+                            let ddd = sock.sendMessage(sock.user.id, { text: e });
+                            let desc = `*_QR CODE Connected*
+🧠 SYSTEM STATUS: ONLINE
+⫸ 𝗞𝗥𝗬𝗣𝗧𝗜𝗫-𝗠𝗗 ⛓️ DEPLOYED SUCCESSFULLY
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+📍 OPERATION LOG:
+✔️ INITIATION COMPLETE
+✔️ BOT FRAMEWORK INJECTED
+✔️ CYBER CORE: STABLE
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+👁‍🗨 SUPPORT HUB:
+⮞ YOUTUBE: youtube.com/@MYSTICHAIKOTECH
+⮞ OWNER LINK: wa.me/50940938083
+⮞ SOURCE: github.com/DEVPRIMIS/KRYPTIX-MD_V1
+⮞ WA CHANNEL: whatsapp.com/channel/0029VbB2Xry0LKZAuosqRO0k
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⭐ STAR THE REPO TO SUPPORT THE CODEVERSE
+💻 BUILT WITH 💙 BY DEV K1NG
+
+> © 𝐊1𝐍𝐆-𝐗𝐌𝐃`; 
+                            await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: " 𝖒𝖉 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉 ✅  ",
+thumbnailUrl: "https://files.catbox.moe/4v0adk.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029VbB2Xry0LKZAuosqRO0k",
+mediaType: 2,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}  
+}
+},
+{quoted:ddd })
+                    }
+                    await delay(10);
+                    await sock.ws.close();
+                    await removeFile('./temp/' + id);
+                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
+                    await delay(10);
+                    process.exit();
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10);
+                    K1NG_XMD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "❗ Service Unavailable" });
+            }
+        }
+    }
+    await K1NG_XMD_PAIR_CODE();
+});
+setInterval(() => {
+    console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
+    process.exit();
+}, 180000); //30min
+module.exports = router;
